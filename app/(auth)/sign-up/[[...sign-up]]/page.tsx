5a432ade2706d1b84cd429e66c74319c7fb91f6f
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-06-16 11:07:37 +0100

    initial comment

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,5 @@
+const SignUpPage = () => {
+  return <div className="">Sign Up Page</div>;
+};
+
+export default SignUpPage;
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+// import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-background">
+      <p>This is an authenticated route</p>
+    </div>
+  );
+}
